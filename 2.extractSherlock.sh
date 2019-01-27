df59a79cbe40d4e934d8af596ead683b64b23c81
vsoch <vsochat@stanford.edu>
2019-01-27 02:54:40 -0800

    no cache

--- 2.extractSherlock.sh
+++ 2.extractSherlock.sh
@@ -5,6 +5,6 @@ OUTPUT="${1}"
 URI="${2}"
 CACHE_DIR="${3}"
 
-container-diff analyze "${URI}" --type=pip --type=apt --type=history --json --quiet --cache-dir "${CACHE_DIR}" --verbosity=panic --output "${OUTPUT}"
+container-diff analyze "${URI}" --type=pip --type=apt --type=history --json --quiet --cache-dir "${CACHE_DIR}" --no-cache --verbosity=panic --output "${OUTPUT}"
 
 # container-diff analyze docker://alerta/alerta-web  --type=pip --type=apt --type=history --json --quiet --cache-dir /scratch/users/vsochat/.singularity/docker --verbosity=panic --output /scratch/users/vsochat/WORK/dockerfiles/container-diff/alerta-alerta-web.json
